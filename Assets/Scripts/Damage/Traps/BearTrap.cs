151d56934fe5957be4743d346bc72e29a09df435
Simon <simon.bahamonde@gmail.com>
2022-09-29 15:24:54 -0300

    Added disarm mechanism for bear traps

--- Assets/Scripts/Damage/Traps/BearTrap.cs
+++ Assets/Scripts/Damage/Traps/BearTrap.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BearTrap : BoxCollider
 {
     private Animator animator;
+    [SerializeField] private List<string> tagsToDisarm;
 
     protected override void Awake()
     {
@@ -15,9 +16,17 @@ public class BearTrap : BoxCollider
 
     protected override void CheckCollision(Collider2D coll, bool boolToAssing)
     {
-        for (int i = 0; i < tagsToCheck.Count; i++)
+        for (int i = 0; i < tagsToDisarm.Count; i++)
         {
-            if (coll.gameObject.CompareTag(tagsToCheck[i]))
+            if (coll.gameObject.CompareTag(tagsToDisarm[i]))
+            {
+                DisarmTrap();
+            }
+        }
+
+        for (int j = 0; j < tagsToCheck.Count; j++)
+        {
+            if (coll.gameObject.CompareTag(tagsToCheck[j]))
             {
                 if (coll.GetComponent<DamageReceiver>().IsAlive())
                     ActivateTrap(coll);
@@ -32,5 +41,11 @@ public class BearTrap : BoxCollider
         coll.SendMessage("ReceiveDamage");
 
         boxCollider.enabled = false;
+    }
+
+    private void DisarmTrap()
+    {
+        boxCollider.enabled = false;
+        animator.SetTrigger(Config.ANIMATOR_BEAR_TRAP_TRIGGER);
     }
 }
