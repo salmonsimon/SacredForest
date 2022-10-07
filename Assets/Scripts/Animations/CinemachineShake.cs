ddb80fbba110558f0c73222010e168c76d0871d7
Simon <simon.bahamonde@gmail.com>
2022-10-07 12:19:29 -0300

    set new virtual camera for shake when loading new scene

--- Assets/Scripts/Animations/CinemachineShake.cs
+++ Assets/Scripts/Animations/CinemachineShake.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.SceneManagement;
 
 public class CinemachineShake : MonoBehaviour
 {
@@ -21,8 +22,27 @@ public class CinemachineShake : MonoBehaviour
 
     private void Start()
     {
+        SetVirtualCamera();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SetVirtualCamera();
+    }
+
+    private void SetVirtualCamera()
+    {
         cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
-        
+
         cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.AddCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0;
         cinemachineBasicMultiChannelPerlin.m_NoiseProfile = noiseSettings;
